6b807d9dbdd085540d80fb70fdaf97ac6d129684
Bùi Nguyễn Hoàng Thọ <thohnb@gmail.com>
2025-11-06 00:01:11 +0700

    Update course page. Can search with filter now

--- src/page/Profile.tsx
+++ src/page/Profile.tsx
@@ -47,7 +47,7 @@ const ProfilePage = () => {
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 mb-8">
             <div>
     {/* #NOTE: Trả về userContext.name => Tên người dùng, userContext.role="Vai trò" Ghi chú:  */}
-              <ProfileCard name={user?.name ?? ""}  mssv="2012345" status={user?.role ?? ""} />
+              <ProfileCard name={user?.name ?? ""}  mssv="2012345" />
             </div>
             <div className="lg:col-span-2">
                 <InfoForm/>
